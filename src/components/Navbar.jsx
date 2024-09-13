c0a6a1cf7692091cd8ab585ef8651d5a995dccdf
MansuraAkhter <mansura3330@gmail.com>
2024-09-14 02:57:40 +0600

    navbar icons added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,18 @@
 "use client";
+import { LinkedInIcon, GithubIcon, MenuIcon } from "@/public/Icons";
+
 const Navbar = () => {
-  return <div>navbar</div>;
+  return (
+    <div className="flex bg-[#0C0C1D]">
+      <div className="bg-white rounded-full p-2">
+        <MenuIcon />
+      </div>
+      <div className="transition ease-in-out   hover:scale-125  duration-300 cursor-pointer ">
+        <LinkedInIcon />
+        <GithubIcon />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
